0e8fda94cadd3156c2c3a63df57e7a66d132699a
MaldeFrank <maldefrankmidtgaard@gmail.com>
2023-09-25 13:12:38 +0200

    date scraping funktion made

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,13 @@
 package org.example;
 
 
+import org.example.Scraping.Date;
 
 public class Main {
     public static void main(String[] args) {
+     Date date = new Date();
 
+     date.dateList().forEach(string-> System.out.println(string));
 
     }
 }
--- src/main/java/org/example/Scraping/Date.java
+++ src/main/java/org/example/Scraping/Date.java
@@ -1,4 +1,19 @@
 package org.example.Scraping;
 
-public class Date {
+import org.jsoup.select.Elements;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Date implements IDate {
+
+    @Override
+    public List<String> dateList() {
+        Elements elements = ScrapingTools.getElements("https://www.accuweather.com/en/dk/copenhagen/123094/daily-weather-forecast/123094", "h2.date span.module-header.sub.date");
+        List<String> dates = elements
+                .stream()
+                .map(element -> element.text())
+                .collect(Collectors.toList());
+        return dates;
+    }
 }
