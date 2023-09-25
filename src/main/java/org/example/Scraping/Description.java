40b5a026749a1ce291413d70d8fd275a8e9e09c9
Mikkel <cph-mh1088@cphbusiness.dk>
2023-09-25 12:35:32 +0200

    Finds and prints all descriptions from url
    Implemented interface and added body to method.

--- src/main/java/org/example/Scraping/Description.java
+++ src/main/java/org/example/Scraping/Description.java
@@ -1,4 +1,34 @@
 package org.example.Scraping;
 
-public class Description {
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Description implements IDescription {
+
+    private static final String url = "https://www.accuweather.com/en/dk/hiller%C3%B8d/123048/daily-weather-forecast/123048";
+    private static final String cssSelector = "div.phrase";
+
+    public static void main(String[] args) {
+        new Description().descList();
+    }
+
+    @Override
+    public List<String> descList() {
+        List<String> descriptionList = new ArrayList<>();
+        Elements descriptions = GetElements.getElements(url, cssSelector);
+
+        for (Element description : descriptions) {
+            String text = description.text();
+            descriptionList.add(text);
+        }
+
+        for (String desc : descriptionList) {
+            System.out.println("Description: " + desc);
+            System.out.println("----------");
+        }
+        return descriptionList;
+    }
 }
