bb1bc2cd71bb3d8a206e6afd9fe49dfe7a6d99c7
Mikkel <cph-mh1088@cphbusiness.dk>
2023-09-25 13:07:34 +0200

    Added javaDoc & test
    Added simple java documentation and a simple unit test

--- src/main/java/org/example/Scraping/Description.java
+++ src/main/java/org/example/Scraping/Description.java
@@ -6,15 +6,24 @@ import org.jsoup.select.Elements;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The Description class is responsible for scraping daily weather descriptions from a website.
+ */
 public class Description implements IDescription {
 
     private static final String url = "https://www.accuweather.com/en/dk/hiller%C3%B8d/123048/daily-weather-forecast/123048";
+    // CSS selector for the weather description
     private static final String cssSelector = "div.phrase";
 
     public static void main(String[] args) {
         new Description().descList();
     }
 
+    /**
+     * The descList method scrapes the daily weather descriptions from a website and returns them as a list of string.
+     *
+     * @return A list of daily weather descriptions.
+     */
     @Override
     public List<String> descList() {
         List<String> descriptionList = new ArrayList<>();
