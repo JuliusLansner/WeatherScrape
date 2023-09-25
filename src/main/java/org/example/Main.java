e9604ff7cff5c8aabe2954966692ef268ac67159
Julius Lansner <axiiss@live.dk>
2023-09-25 11:52:01 +0200

    Added function to scrape and return to a List of temps, in high/low

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,13 @@
 package org.example;
 
 
+import org.example.Scraping.Temp;
 
 public class Main {
     public static void main(String[] args) {
+        Temp temp = new Temp();
 
+        System.out.println(temp.tempList());
 
     }
 }
--- src/main/java/org/example/Scraping/Temp.java
+++ src/main/java/org/example/Scraping/Temp.java
@@ -1,4 +1,34 @@
 package org.example.Scraping;
 
-public class Temp {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Temp implements ITemp{
+    @Override
+    public List<String> tempList() {
+        String url = "https://www.accuweather.com/en/dk/hiller%C3%B8d/123048/daily-weather-forecast/123048";
+        List<String> htlt;
+        try {
+            final Document document = Jsoup.connect(url).get();
+            htlt = new ArrayList<>();
+
+            Elements temps = document.select(".temp");
+            for (Element t : temps) {
+                String highTemps = t.select(".high").text();
+                String lowTemps = t.select(".low").text();
+                String combined = highTemps + lowTemps;
+                htlt.add(combined);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return htlt;
+    }
 }
