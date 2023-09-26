a90f8c7e335cd59a87427b81e13d52be531bcf96
Julius Lansner <axiiss@live.dk>
2023-09-26 14:25:36 +0200

    added malde api

--- src/main/java/org/example/api/weatherAPIM.java
+++ src/main/java/org/example/api/weatherAPIM.java
@@ -1,5 +1,12 @@
 package org.example.api;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 import java.io.IOException;
 
 public class weatherAPIM {
