13f15bec24b84ef704c2e9cdc51cef35e30fe930
Julius Lansner <axiiss@live.dk>
2023-09-28 09:57:22 +0200

    Spelling error

--- src/main/java/MainTomorrow.java
+++ src/main/java/MainTomorrow.java
@@ -4,8 +4,8 @@ import org.example.DAO.GetTomorrow;
 
 public class MainTomorrow {
     public static void main(String[] args) {
-        GetTomorrow getTomrrorw = new GetTomorrow();
-        WeatherEntity tomorrow = getTomrrorw.getTomorrow();
+        GetTomorrow getTomorrow = new GetTomorrow();
+        WeatherEntity tomorrow = getTomorrow.getTomorrow();
         System.out.println(tomorrow);
 
 
